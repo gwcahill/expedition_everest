bb5de4466a1f1f29660e236c1b062caf7db3f3bb
Grant Cahill <grant.cahill@sv.cmu.edu>
2012-09-15 11:06:53 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExpeditionEverest::Application.config.secret_token = 'efa529d01fa5c656778f4be74ed7639605cf832f0f067bec9cb20edcaa38c785f03e9ed404349eb21f8dfdc6b1cac0ce2b0f13f7c53f3e622d5347f5b11813ee'
