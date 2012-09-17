a63f6598d62e6280c617fce2581207f802c4f10b
Grant Cahill <grant.cahill@sv.cmu.edu>
2012-09-16 22:33:36 -0600

    Completed layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 ExpeditionEverest::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/news"
-  get "static_pages/imagineering"
+  root to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/news',    to: 'static_pages#news'
+  match '/imagineering', to: 'static_pages#imagineering'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,68 +2,41 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the h1 'Imagineering Everest'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Imagineering Everest')
+    it { should have_selector('h1',    text: 'Welcome to Imagineering Everest') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
     end
-
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "Imagineering Everest | Home")
-    end
-  end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => "Imagineering Everest | Help")
-    end
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the h1 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About')
-    end
-
-    it "should have the title 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => "Imagineering Everest | About")
-    end
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About')) }
   end
 
   describe "News page" do
+    before { visit news_path }
 
-    it "should have the h1 'News'" do
-      visit '/static_pages/news'
-      page.should have_selector('h1', :text => 'News')
-    end
-
-    it "should have the title 'News'" do
-      visit '/static_pages/news'
-      page.should have_selector('title', :text => "Imagineering Everest | News")
-    end
+    it { should have_selector('h1',    text: 'News') }
+    it { should have_selector('title', text: full_title('News')) }
   end
 
   describe "Imagineering" do
+    before { visit imagineering_path }
 
-    it "should have the h1 'Imagineering'" do
-      visit '/static_pages/imagineering'
-      page.should have_selector('h1', :text => 'Imagineering')
-    end
-
-    it "should have the title 'Imagineering'" do
-      visit '/static_pages/imagineering'
-      page.should have_selector('title', :text => "Imagineering Everest | Imagineering")
-    end
+    it { should have_selector('h1',    text: 'Imagineering') }
+    it { should have_selector('title', text: full_title('Imagineering')) }
   end
 end
