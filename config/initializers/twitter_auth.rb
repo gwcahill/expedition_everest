8cb514cf37bbd8e24cd6846bef7636a74a7c79a8
Grant Cahill <grant.cahill@sv.cmu.edu>
2012-10-01 08:28:48 -0700

    updated twitter auth

--- config/initializers/twitter_auth.rb
+++ config/initializers/twitter_auth.rb
@@ -1,6 +1,6 @@
 Twitter.configure do |config|
-  config.consumer_key = "V0DQrjrYmkMejQwJylkCA"
-  config.consumer_secret = "Ja97W7X6jintLG6KErFRAc8PLkrwpLQxcO4o1lro"
-  config.oauth_token = "303593467-Fybmv0XBeIVTK8u7mbUrhjfTRtn66wVBJfJaB6Nc"
-  config.oauth_token_secret = "uRD4n0KPVjCtQIAIfiRMcG7cyCWYX4hJQqQAxDHQeI"
+  config.consumer_key = "ppjSgfLqpUqm0Shh6KTq3w"
+  config.consumer_secret = "muG6NQySpCDDXvgkX0PqdAC3KzBtD0n2XCrH6Go"
+  config.oauth_token = "856536350-9CKrKAc4x3RqKh8N9tNN51GcsvkIlhKlSayPnCQh"
+  config.oauth_token_secret = "Sx5uL3gnsSpnmcX7XfA5GMPpWJ36VmNO2T8zjfSkLU"
 end
