c2ecf30da749190880784cb2040f5ec5dd48116a
Grant Cahill <grant.cahill@sv.cmu.edu>
2012-10-01 12:52:00 -0700

    log in and out work completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ ExpeditionEverest::Application.routes.draw do
   get "expedition_everest_tweets/everesttweets"
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   root to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
@@ -9,5 +14,6 @@ ExpeditionEverest::Application.routes.draw do
   match '/news',    to: 'static_pages#news'
   match '/imagineering', to: 'static_pages#imagineering'
   match '/joinnow', to: 'users#new'
+
   match '/everesttweets', to: 'ExpeditionEverestTweets#everesttweets'
 end
