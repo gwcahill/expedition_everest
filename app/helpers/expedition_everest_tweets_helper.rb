9447d3cd1783b2793b2f683503732754d0f76632
Grant Cahill <grant.cahill@sv.cmu.edu>
2012-10-03 18:13:44 -0700

    fixed tweet output

--- app/helpers/expedition_everest_tweets_helper.rb
+++ app/helpers/expedition_everest_tweets_helper.rb
@@ -5,6 +5,11 @@ module ExpeditionEverestTweetsHelper
     #Twitter.search("#expeditioneverest", :count => 20, :result_type => "recent").results.map do |status|
     #  "#{status.from_user}: #{status.text}"
     #end
-    Twitter.search("#expeditioneverest", :count => 1).results.first.text
+    #Twitter.search("#expeditioneverest", :count => 1).results.first.text
+    @tweets = []
+    Twitter.search("#expeditioneverest", :count => 15, :result_type => "recent").results.map do |status|
+      @tweets << "#{status.from_user}: #{status.text}"
+    end
+    @tweets
   end
 end
